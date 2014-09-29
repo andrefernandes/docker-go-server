d74ee70ce0edd80af0889c72f73e23d0f23bda98
andre <goroh@hotmail.com>
2014-09-29 08:52:31 -0300

    fixes

--- rungo.sh
+++ rungo.sh
@@ -1,7 +1,7 @@
 GOPORT=${GOPORT:-8153}
 GOSPORT=${GOSPORT:-8154}
 echo "Will use ports $GOPORT and $GOSPORT"
-docker run --rm -t -i \
+docker run --name go-server -d \
    -p $GOPORT:8153 \
    -p $GOSPORT:8154 \
    -v /opt/go:/opt/go \
